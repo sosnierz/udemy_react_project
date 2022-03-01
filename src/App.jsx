11d2d8a1fb1ad9c709c6cf75feaca8fd71023a8e
sosnierz <37373004+sosnierz@users.noreply.github.com>
2022-03-01 13:34:56 +0100

    feat: implement of app store and request helper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import StoreProvider from './store/StoreProvider';
+
 import './App.scss';
 
 const App = () => {
     return (
+      <StoreProvider>
         <header>Hello</header>
+        </StoreProvider>
       );
 }
  
